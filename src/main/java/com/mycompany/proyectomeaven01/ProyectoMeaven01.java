0a1afd729e307b23b91db64bf63e418e2fcf089a
Eduardo Rodríguez <147782137+EduardoRM777@users.noreply.github.com>
2023-10-13 00:20:42 -0600

    Creación de practica P1 meaven

---
+++ src/main/java/com/mycompany/proyectomeaven01/ProyectoMeaven01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectomeaven01;
+
+/**
+ *
+ * @author Eduardo Rodriguez Morales
+ */
+public class ProyectoMeaven01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde POO, grupo 8, desde Meaven");
+    }
+}
